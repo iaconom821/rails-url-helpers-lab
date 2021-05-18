54e74031a9e9ef42b67106430a8aadb5f5e9af42
iaconom821 <iaconom821@gmail.com>
2021-05-18 18:03:14 -0400

    commit tree afb6c1a17c5f7541020a1c47e84a8e13fdb3bd70

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,19 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+    if @student.active 
+      @active = "This student is currently active."
+    else
+      @active = "This student is currently inactive."
+    end
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.update(active: !@student.active)
+    
+    redirect_to student_path(@student) 
   end
 
   private
